266bdff9eb624ebcd38c491ec759227d107150fb
Asta86 <psljp@protonmail.com>
2024-05-09 13:14:49 -0300

    Move examples to scripts, functions without examples yet to main

--- src/python/main.py
+++ src/python/main.py
@@ -1,22 +1,10 @@
-from backtracking.sudoku_solver import solve as solvesudoku
-from graphs.maze_solver import solve as solvemaze
+from backtracking.wolf_goat_and_cabbage import solve as solvewgc
+from dynamic_programming.cheapest_path_in_matrix import cheapest_path
+from dynamic_programming.paths_in_grid import grid_paths
 
-g9 = [
-    [None, 1, 2, None, None, 6, 5, 8, 9],
-    [6, 7, None, None, None, None, None, None, 2],
-    [None, 8, 4, 1, None, 5, 6, 3, None],
-    [4, 3, 8, 2, 5, 9, 1, None, None],
-    [None, None, None, None, None, None, None, None, None],
-    [None, None, 7, 8, 1, 4, 3, 9, 5],
-    [None, 9, 6, 5, None, 1, 7, 2, None],
-    [7, None, None, None, None, None, None, 5, 1],
-    [5, 4, 1, 7, None, None, 8, 6, None]
-]
-print(solvesudoku(g9).pop())
+# move to separate files with examples
+print("Solution for Wolf, Goat and Cabbage problem:", solvewgc([False, False, False, False], []))
 
-m0 = [
-    ["S"],
-    ["E"]
-]
-print(solvemaze(m0))
+print(cheapest_path([[1, 2, 2], [1, 3, 4]]))
 
+print(grid_paths(6, 8))
